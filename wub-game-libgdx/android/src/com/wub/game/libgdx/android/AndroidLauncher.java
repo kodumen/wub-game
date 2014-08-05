025a6c9319157360973e2b734cbae2e02756ba9d
kodumen <r.b.daitan@gmail.com>
2014-08-05 15:35:03 +0800

    Added libgdx project files
    I might use libgdx so I'll add them right now.

---
+++ wub-game-libgdx/android/src/com/wub/game/libgdx/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.wub.game.libgdx.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.wub.game.libgdx.WubGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WubGame(), config);
+	}
+}
