05495a14ed3b0f47b822e2a93290f54be3ccd474
kodumen <r.b.daitan@gmail.com>
2014-08-15 00:07:27 +0800

    Revert "Modified randomization"
    This reverts commit 1298e243c696ac73251ef9305515455359dea39d.

--- wub-game-libgdx/core/src/com/wub/game/libgdx/Behavior/ItemType.java
+++ wub-game-libgdx/core/src/com/wub/game/libgdx/Behavior/ItemType.java
@@ -48,7 +48,7 @@ public class ItemType extends GameComponent {
     }
 
     public void randomizeType() {
-        setType((int)(Math.random() * (BOMB - SCORE1 + 1)) + SCORE1);
+        setType((int)(Math.random() * (BOMB - NONE + 1)) + NONE);
         setTextureFromType();
     }
 
